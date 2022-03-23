14ca045a2b201b54766ea81332fde6c963e00552
dianapaolam <100746459+dianapaolam@users.noreply.github.com>
2022-03-23 11:00:49 -0600

    Add files via upload

---
+++ Distribuciones.R
@@ -0,0 +1,21 @@
+
+# Probabilidad distribuciones
+
+#Suponga que se desea saber el número de llegadas de los estudiantes de una 
+# Escuela Primaria en un lapso de 15 minutos a la entrada de la escuela.
+
+#El número promedio de alumnos que llegan en un lapso de 15 minutos son 35.
+# x=35(número de alumnos) lamda=15 (tiempo de llegada)
+dpois(35,15)
+
+#Resultados P(A) 0.0000043 probabilidad nula
+
+#x=50  lamda=15
+dpois(50,15)
+
+#Resultados  P(A) 0.00000000000064 probabilidad nula
+
+# X=50 lamda=2
+dpois(50,2)
+dpois(3,5)
+dpois(5,10)
